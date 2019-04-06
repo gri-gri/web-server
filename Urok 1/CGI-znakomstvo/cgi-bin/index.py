c8a1cab1b382a6df5f7f8b9cfc8efaacdd6e4cce
Gri-Gri <gri_simonov@mail.ru>
2019-04-06 12:44:44 +0300

    Added first lesson, test programm and programm 1

---
+++ Urok 1/CGI-znakomstvo/cgi-bin/index.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+ 
+ 
+print("Content-type: text/html; charset=utf-8")
+print()
+print("<h1>Привет, Яндекс! Я - Гриша</h1>")
---
+++ Urok 1/test/cgi-bin/index.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+ 
+ 
+print("Content-type: text/html; charset=utf-8")
+print()
+print("<h1>Hello, Yandex!</h1>")
